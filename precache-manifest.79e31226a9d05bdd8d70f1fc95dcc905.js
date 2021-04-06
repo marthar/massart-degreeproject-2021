2b16cf48083794d2f9508bc8818d00179c476947
Martha Rettig <marthar@cykod.com>
2021-04-05 20:52:14 -0400

    Updates

---
+++ precache-manifest.79e31226a9d05bdd8d70f1fc95dcc905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f4b8bd510514485376f561d364fa4f",
+    "url": "/massart-degreeproject/index.html"
+  },
+  {
+    "revision": "f7edba4d455de1425658",
+    "url": "/massart-degreeproject/static/css/main.07265319.chunk.css"
+  },
+  {
+    "revision": "476b115337ddd2de6abf",
+    "url": "/massart-degreeproject/static/js/2.39c6b036.chunk.js"
+  },
+  {
+    "revision": "d231813f6fb47aa56cf6f9aa1606f022",
+    "url": "/massart-degreeproject/static/js/2.39c6b036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7edba4d455de1425658",
+    "url": "/massart-degreeproject/static/js/main.16a52af7.chunk.js"
+  },
+  {
+    "revision": "07db68b2e21e7ac42a22",
+    "url": "/massart-degreeproject/static/js/runtime-main.6f31538b.js"
+  }
+]);
